3bb665f7ca2438c018ad3af330d3931a94a65df7
Sarvarbek Salohiddinoff <79306205+8ar7ar@users.noreply.github.com>
2022-06-21 09:40:44 +0500

    update app.py

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ print('Hello world')
 print('Hello world')
 print('Hello world')
 print('Hello world')
+#dpwojdpowmdpnwmdnwondw
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+#dpwojdpowmdpnwmdnwondw
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+#dpwojdpowmdpnwmdnwondw
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+#dpwojdpowmdpnwmdnwondw
